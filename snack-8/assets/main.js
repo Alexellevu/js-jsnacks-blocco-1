6f2cebfa0bf6de84249deefd9ccf7a3abc621ad2
alexellevu <newnapster83@gmail.com>
2021-04-28 16:33:24 +0200

    inserimento numero in array

---
+++ snack-8/assets/main.js
@@ -0,0 +1,16 @@
+//Chiedi un numero di 4 cifre all’utente
+//e calcola la somma di tutte le cifre che compongono il numero
+
+var numUtente = (prompt('inserisci un numero di 4 cifre'));
+console.log(numUtente);
+var array = [];
+
+
+array.push(parseInt(numUtente));
+console.log(array);
+
+for(var i=0; i < array.length; i++)
+{
+var somma = somma + array[i];
+console.log(somma);
+}
